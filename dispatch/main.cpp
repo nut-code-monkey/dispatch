cd1fbd24d7040a65812c9c3a67972e2f204eec06
Max Lunin <nut.code.monkey@gmail.com>
2013-06-30 15:44:20 +0300

    Initial Commit

---
+++ dispatch/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  dispatch
+//
+//  Created by Max on 30.06.13.
+//  Copyright (c) 2013 Max Lunin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

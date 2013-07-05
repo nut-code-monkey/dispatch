32eaec7a44cd91a5c38164ffec606974d38affbd
Max Lunin <nut.code.monkey@gmail.com>
2013-07-05 15:54:41 +0300

    update main

--- dispatch/main.cpp
+++ dispatch/main.cpp
@@ -14,24 +14,29 @@
 
 int main(int argc, const char * argv[])
 {
-    std::string hello("hello");
-    
     dispatch::Queue queue = dispatch::get_queue_with_priority(dispatch::QUEUE_PRIORITY::HIGH);
 
     dispatch::Queue main_queue = dispatch::get_main_queue();
     for (int i = 0; i < 20; ++i)
     {
         dispatch::async(queue, [=](){
-            int res = i + 1;
+            
+            std::string first_string = std::to_string(i);
+
             dispatch::async(main_queue, [=]{
-                std::cout << res << std::endl;
+                
+                std::string second_string = std::to_string(i+1);
+                
+                std::cout << first_string << " -> " << second_string << std::endl;
             });
         });
     }
 
-    dispatch::async(dispatch::get_queue_with_priority(dispatch::QUEUE_PRIORITY::BACKGROUND), []{
-        
-        dispatch::exit();
+    dispatch::async(dispatch::get_queue_with_priority(dispatch::QUEUE_PRIORITY::BACKGROUND), [=]{
+        dispatch::async(main_queue, [](){
+            std::cout << "exit" << std::endl;
+            dispatch::exit();
+        });
     });
 
     dispatch::main_loop([]{
